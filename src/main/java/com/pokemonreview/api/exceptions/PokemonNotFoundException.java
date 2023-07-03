420c7416c9f8ab185ac89f3f8761f69e35bd2966
ypatoglu <ypatoglu@icloud.com>
2023-07-03 23:47:08 +0300

    update exception

--- src/main/java/com/pokemonreview/api/exceptions/PokemonNotFoundException.java
+++ src/main/java/com/pokemonreview/api/exceptions/PokemonNotFoundException.java
@@ -1,7 +1,7 @@
 package com.pokemonreview.api.exceptions;
 
 public class PokemonNotFoundException extends RuntimeException {
-    private static final long serialVerisionUID = 1;
+    private static final long serialVersionUID = 1;
 
     public PokemonNotFoundException(String message) {
         super(message);
